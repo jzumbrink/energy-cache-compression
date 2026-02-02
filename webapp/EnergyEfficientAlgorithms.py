430a51ba3950744c3e1e98d3366c072009be4a4d
Jan Zumbrink <j.zumbrink@gmx.de>
2026-02-02 12:57:56 +0100

    first part of webapp

---
+++ webapp/EnergyEfficientAlgorithms.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import shared.streamlit_utils as su
+
+su.c_set_page_config()
+
+st.title("Energy Efficient Algorithms")
---
+++ webapp/shared/streamlit_utils.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from energy_efficient_algorithms.compression_emissions import *
+
+def c_set_page_config(page_title="Energy Efficient Algorithms"):
+    st.set_page_config(
+        layout="wide",
+        page_title=page_title,
+        page_icon="⚡")
+
+def create_compressed_text_idx_plot(compressed_text_idx, filename, indices=tuple([10**(i+2) for i in range(11)])):
+    disk_emissions = disk_co2_emissions(idx_sizes[filename][compressed_text_idx])
+    query_emissions = [query_co2_emissions(c, query_energy_usage[filename][compressed_text_idx]) for c in indices]
+
+    query_emissions_series = pd.Series(
+        query_emissions,
+        index=indices,
+        name="Emissions of queries"
+    )
+
+    disk_emissions_series = pd.Series(
+        [disk_emissions for _ in range(len(query_emissions))],
+        index=indices,
+        name="Emissions of disk usage"
+    )
+
+    total_emissions_series = pd.Series(
+        [disk_emissions + qe for qe in query_emissions],
+        index=indices,
+        name="Total emissions"
+    )
+
+    fig, ax = plt.subplots()
+    query_emissions_series.plot(ax=ax, marker="o", logy=True, logx=True)
+    disk_emissions_series.plot(ax=ax, marker="o", logy=True, logx=True)
+    total_emissions_series.plot(ax=ax, marker="o", logy=True, logx=True)
+
+    ax.set_xlabel("Queries")
+    ax.set_ylabel("CO2 eq [g]")
+    ax.legend()
+
+    return fig
